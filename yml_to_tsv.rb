6cb260b06d3b102bf36c195096ddb1f5a0b03b5a
syy41151 <40396787+syy41151@users.noreply.github.com>
2018-09-23 11:40:12 +0800

    Update yml_to_tsv.rb

--- yml_to_tsv.rb
+++ yml_to_tsv.rb
@@ -1,1 +1,31 @@
+require 'yaml'
+
+@YML_DATA = YAML.safe_load(File.read(ARGV[0]))
+TSV = File.new(ARGV[1],"w")
+
+Test = []
+Title = ""
+DATA = ""
+
+@YML_DATA[0].each {|x| Test << x}
+for i in 0..(Test.length-1)
+    Title << Test[i][0]+"\t"
+end
+TSV.write Title 
+TSV.write "\n"
+
+for i in 0..(@YML_DATA.length-1)
+    DATA.clear
+    Test.clear
+    @YML_DATA[i].each {|x| Test << x}
+        for j in 0..(Test.length-1)
+            DATA << Test[j][1]+"\t"
+        end
+    TSV.write DATA 
+    TSV.write "\n"
+end
+
+TSV.close
+Show = File.read(ARGV[1])
+puts Show 
 
